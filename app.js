3ca807fe88c1fa6edecfa52573460a6ce1d1d170
Anshuman <Anshumant72@gmail.com>
2022-07-19 15:17:11 +0530

    done

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.get("/contact", function(req, res){
 });
 
 let port = process.env.PORT;
-if (port == null || port="") {
+if (port == null || port=="") {
   port=3000;
 }
 app.listen(port, function() {
